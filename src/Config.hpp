898f1a54e984303226b313f8d6eac6bacff58bf0
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-09-13 23:28:38 +0200

    Config and Quadtree documentation

--- src/Config.hpp
+++ src/Config.hpp
@@ -11,8 +11,12 @@
 #include <SFML/System/Time.hpp>
 
 
+/** User's configuration file name. */
 const sf::String CONFIG_FILE        ("config");
 
+
+/** @name Application's window configuration */
+/**@{*/
 
 const sf::String WINDOW_TITLE       ("Game name here");
 const sf::String WINDOW_ICON        ("res/icon.png");
@@ -23,6 +27,17 @@ const bool       WINDOW_BORDERLESS  = false;
 const bool       WINDOW_VSYNC       = true;
 const unsigned   WINDOW_FRAMERATE   = 60;
 
+/**@}*/
+
+
+/** @name Pause screen configuration */
+/**@{*/
+
+const sf::String PAUSE_TITLE        ("PAUSE");
+const sf::String PAUSE_SUBTITLE     ("Press P to resume the game");
+
+/**@}*/
+
 
 // Maximum "elapsed time" in update function
 // This value means (if set to 40 ms) that the game simulation will
@@ -30,15 +45,19 @@ const unsigned   WINDOW_FRAMERATE   = 60;
 const sf::Time   UPDATE_MAX_DELTA   (sf::milliseconds(40));
 
 
+/** @name Intro screen configuration */
+/**@{*/
+
 const sf::Time   INTRO_TIME         (sf::seconds(5.f));
 const sf::Time   INTRO_TRANSITION   (sf::seconds(.5f));
 const sf::String INTRO_TITLE        (WINDOW_TITLE);
 const sf::String INTRO_SUBTITLE     ("Made by Bastien Brunnenstein");
 
+/**@}*/
 
-const sf::String PAUSE_TITLE        ("PAUSE");
-const sf::String PAUSE_SUBTITLE     ("Press P to resume the game");
 
+/** @name Resources and workers configuration */
+/**@{*/
 
 const bool       RESOURCES_THREADING    = true;
 const unsigned   RESOURCES_WORKERS_MAX  = 8u;   // 1 min
@@ -46,6 +65,11 @@ const unsigned   RESOURCES_JOBS_MAX     = 128u; // 32 min
 
 const sf::String RESOURCES_DEFAULT_FONT ("res/fonts/PoetsenOne-Regular.ttf");
 
+/**@}*/
+
+
+/** @name Main menu configuration */
+/**@{*/
 
 const sf::String MENU_BACKGROUND    ("res/menu.png");
 
@@ -53,7 +77,10 @@ const sf::Color  MENU_ITEM          (sf::Color::Black);
 const sf::Color  MENU_SHADOW        (sf::Color::White);
 const sf::Color  MENU_SELECTION     (0, 0, 192);
 
-const bool       MENU_LOAD_ENABLED  = true;
+/** Set to false to completely disable loading a previously saved game */
+const bool       MENU_LOAD_ENABLED    = true;
+
+/** Set to false to completely disable options screen */
 const bool       MENU_OPTIONS_ENABLED = true;
 
 const sf::String MENU_NEW_GAME      ("New game");
@@ -62,8 +89,8 @@ const sf::String MENU_OPTIONS       ("Options");
 const sf::String MENU_QUIT          ("Quit");
 
 const sf::String MENU_RESOLUTION    ("Resolution");
-const sf::String MENU_MODE          ("Mode");
 
+const sf::String MENU_MODE          ("Mode");
 const sf::String MENU_WINDOWED      ("< Windowed >");
 const sf::String MENU_BORDERLESS    ("< Borderless >");
 const sf::String MENU_FULLSCREEN    ("< Fullscreen >");
@@ -74,23 +101,33 @@ const sf::String MENU_CANCEL        ("Cancel");
 const sf::String MENU_YES           ("Yes");
 const sf::String MENU_NO            ("No");
 
-
-const unsigned   QUADTREE_MINIMUM_SIZE = 4u; // 1 min
-const unsigned   QUADTREE_BEST_DEPTH   = 3u; // found after some tests
+/**@}*/
 
 
-const sf::String ERROR_STRING       ("<<<ERROR>>>");
-
-
+/**
+ * Special exceptions that can be thrown, usually from screens.
+ *
+ * They are caught by the main loop.
+ */
 typedef enum Exception
 {
+    /**
+     * Exception that cause the application to be closed.
+     */
     EXCEPTION_EXIT,
+
+    /**
+     * Exception that cause the re-creation of game windows when thrown.
+     *
+     * Used to change the game resolution or window mode.
+     * For example, you can't switch to fullscreen without closing the window.
+     */
     EXCEPTION_RESTART,
 
 } Exception;
 
 
-// Configuration variables, implemented in Main.cpp
+/** Configuration variables and functions. */
 namespace Config
 {
     extern unsigned   width;        // = WINDOW_WIDTH
@@ -98,7 +135,10 @@ namespace Config
     extern bool       fullscreen;   // = WINDOW_FULLSCREEN
     extern bool       borderless;   // = WINDOW_BORDERLESS
 
+    /** Save current configuration in CONFIG_FILE. */
     void save();
+
+    /** Load configuration from CONFIG_FILE. */
     void load();
 
 }
--- src/game/Quadtree.hpp
+++ src/game/Quadtree.hpp
@@ -8,41 +8,113 @@
 
 #include <vector>
 
-#include <SFML/Graphics/Rect.hpp>
-
 class Entity;
 
+#include <SFML/Graphics/Rect.hpp>
+
+
+/**
+ * Quadtree division's minimum width and height.
+ *
+ * This is not really a minimum as the size can be under it.
+ * A quadtree will not be subdivised furthermore if either it's width or height
+ * is inferior or equal to this value.
+ *
+ * Minimum value : 1, default value : 4
+ */
+const unsigned   QUADTREE_MINIMUM_SIZE  = 4u;
+
+/**
+ * Default depth value for a Quadtree.
+ *
+ * This value is used as a default parameter and can be overrided manually.
+ * A smaller depth result in more pairs created, while a higher depth result
+ * in less pairs created, but a higher cost to insert elements into the Quadtree.
+ *
+ * A value between 2 and 4 should be used.
+ *
+ * Default value : 3
+ */
+const unsigned   QUADTREE_DEFAULT_DEPTH = 3u;
+
+
+/**
+ * Space partitionner using a quadtree.
+ *
+ * Allow a quicker resolution of collisions by grouping entities spatially.
+ */
 class Quadtree
 {
 public:
     typedef void (*CallbackFunction) (const Entity * first, const Entity * second);
 
-    // depth is the maximum depth of the quadtree
-    // You can have up to (4 ^ depth) subdivisions in a quadtree
-            Quadtree(const sf::IntRect & bounds, unsigned depth);
+    /**
+     * Create a Quadtree with given bounds and depth.
+     *
+     * If an entity is out of bounds, it will still be able to collide other
+     * entities, but this case should be an exception.
+     * You can have up to (4 ^ depth) subdivisions in a quadtree.
+     *
+     * @param bounds Square that should be enclosing every entity.
+     * @param depth is the maximum depth of the quadtree
+     */
+            Quadtree(const sf::IntRect & bounds, unsigned depth = QUADTREE_DEFAULT_DEPTH);
 
             ~Quadtree();
 
+    /**
+     * Insert an entity into the quadtree.
+     *
+     * This function will create sub-quadtrees as needed.
+     *
+     * @param entity
+     */
     void    pushEntity(const Entity * entity);
 
-    // Call a function for every pair of entities that could possibly collides
+    /**
+     * Call a function for every pair of entities that could possibly collides.
+     *
+     * @param callback_function
+     */
     void    process(CallbackFunction callback_function) const;
 
-    // Call a function for every entity that could possibly collide with given entity
-    // The entity given to this function is passed as the first parameter of callback function
+    /**
+     * Call a function for every entity that could possibly collide with given entity.
+     *
+     * The entity given to this function is passed as the first parameter of callback function.
+     *
+     * @param entity
+     * @param callback_function
+     */
     void    processSingleEntity(const Entity * entity, CallbackFunction callback_function) const;
 
 private:
     typedef std::vector<const Entity *> EntityList;
     typedef EntityList::const_iterator  EntityIterator;
 
-    // entity_bounds is cached by pushEntity
+    /**
+     * Internal function used to put entity_bounds in cache.
+     *
+     * @see pushEntity
+     *
+     * @param entity
+     * @param entity_bounds entity's @c integer bounds, enclosing it's @c float bounds.
+     */
     void    pushEntityInternal(const Entity * entity, const sf::IntRect & entity_bounds);
 
-    // entity_bounds is cached by processSingleEntity
+    /**
+     * Internal function used to put entity_bounds in cache.
+     *
+     * @see processSingleEntity
+     *
+     * @param entity
+     * @param callback_function
+     * @param entity_bounds entity's @c integer bounds, enclosing it's @c float bounds.
+     */
     void    processSingleEntityInternal(const Entity * entity, CallbackFunction callback_function, const sf::IntRect & entity_bounds) const;
 
     sf::IntRect     _bounds;
+
     int             _vertical_middle;
     int             _horizontal_middle;
 
