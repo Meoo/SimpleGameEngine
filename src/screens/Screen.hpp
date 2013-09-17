dcee64fd33a91df20d28a6b9d990c1bbea4013cf
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-09-17 19:15:49 +0200

    Screen.hpp doxygen documentation

--- src/screens/Screen.hpp
+++ src/screens/Screen.hpp
@@ -8,16 +8,39 @@
 
 #include <SFML/Graphics.hpp>
 
+/**
+ * A screen is a "state" for the application.
+ *
+ * Screens controls the flow of an application.
+ */
 class Screen : public sf::Drawable
 {
 public:
+    /**
+     * Virtual destructor for inheritance.
+     */
     virtual ~Screen() {}
 
-    // Return next screen to use or NULL
-    // If something else than NULL is returned the Screen should be considered as an invalid object
-    // It means you can safely "delete this" right before returning the next Screen
+    /**
+     * Update the scene.
+     *
+     * If you return something else than the null pointer, it will be used
+     * as the next screen for the application.
+     *
+     * @warning The memory is not managed automatically, so if you don't pass
+     *          this screen as a parameter for the new one, then you probably
+     *          should "delete this" right before returning.
+     *
+     * @param elapsed_time
+     * @return next screen to be used, or null pointer.
+     */
     virtual Screen * update(sf::Time elapsed_time) = 0;
 
+    /**
+     * Callback to handle key presses.
+     *
+     * @param key
+     */
     virtual void onKeyPressed(sf::Keyboard::Key key) {}
 
 };
