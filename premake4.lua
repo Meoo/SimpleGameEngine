3192eab2919b282e4cd7ce00bd0be3963a961e64
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-09-20 17:05:06 +0200

    Corrected indentation on premake4 file

--- premake4.lua
+++ premake4.lua
@@ -1,75 +1,75 @@
 
 -- SFML2 options
 newoption {
-   trigger     = "sfml_directory",
-   value       = "path",
-   description = "SFML2 base directory (must contains include directory)"
+  trigger     = "sfml_directory",
+  value       = "path",
+  description = "SFML2 base directory (must contains include directory)"
 }
 
 if not _OPTIONS["sfml_directory"] then
-   _OPTIONS["sfml_directory"] = ""
+  _OPTIONS["sfml_directory"] = ""
 end
 
 newoption {
-   trigger     = "sfml_bin_directory",
-   value       = "path",
-   description = "[Optional] SFML2 binaries directory (must contains lib directory)"
+  trigger     = "sfml_bin_directory",
+  value       = "path",
+  description = "[Optional] SFML2 binaries directory (must contains lib directory)"
 }
 
 if not _OPTIONS["sfml_bin_directory"] then
-   _OPTIONS["sfml_bin_directory"] = _OPTIONS["sfml_directory"]
+  _OPTIONS["sfml_bin_directory"] = _OPTIONS["sfml_directory"]
 end
 
 newoption {
-   trigger     = "sfml_static",
-   value       = "bool",
-   description = "[Default=false] Link SFML2 as a static library",
-   allowed = {
-      { "true",   "Static linkage enabled" },
-      { "false",  "Static linkage disabled" },
-   }
+  trigger     = "sfml_static",
+  value       = "bool",
+  description = "[Default=false] Link SFML2 as a static library",
+  allowed = {
+    { "true",   "Static linkage enabled" },
+    { "false",  "Static linkage disabled" },
+  }
 }
 
 -- Output directories option
 newoption {
-   trigger     = "output_directory",
-   value       = "path",
-   description = "[Default=bin] Output directory for the executable"
+  trigger     = "output_directory",
+  value       = "path",
+  description = "[Default=bin] Output directory for the executable"
 }
 
 if not _OPTIONS["output_directory"] then
-   _OPTIONS["output_directory"] = "bin"
+  _OPTIONS["output_directory"] = "bin"
 end
 
 -- GNU GProf option
 newoption {
-   trigger     = "gprof",
-   value       = "bool",
-   description = "[Default=false] Enable GNU Profiler file generation",
-   allowed = {
-      { "true",   "GNU Profiler enabled" },
-      { "false",  "GNU Profiler disabled" },
-   }
+  trigger     = "gprof",
+  value       = "bool",
+  description = "[Default=false] Enable GNU Profiler file generation",
+  allowed = {
+    { "true",   "GNU Profiler enabled" },
+    { "false",  "GNU Profiler disabled" },
+  }
 }
 
 -- Static stdlib linkage option
 newoption {
-   trigger     = "stdlib_static",
-   value       = "bool",
-   description = "[Default=false] Enable static linking of standard libraries",
-   allowed = {
-      { "true",   "Static linking of stdlib enabled" },
-      { "false",  "Static linking of stdlib disabled" },
-   }
+  trigger     = "stdlib_static",
+  value       = "bool",
+  description = "[Default=false] Enable static linking of standard libraries",
+  allowed = {
+    { "true",   "Static linking of stdlib enabled" },
+    { "false",  "Static linking of stdlib disabled" },
+  }
 }
 
 -- ///////////////////////////////////////////////////// --
 
 function bool_default(value, default)
-    if not value then return default end
-    if value:lower() == "true" then return true end
-    if value:lower() == "false" then return false end
-    error (value.." is not a valid boolean value")
+  if not value then return default end
+  if value:lower() == "true" then return true end
+  if value:lower() == "false" then return false end
+  error (value.." is not a valid boolean value")
 end
 
 -- SFML2 directories and configuration
@@ -90,10 +90,10 @@ local STDLIB_STATIC = bool_default(_OPTIONS["stdlib_static"], false)
 
 -- SFML2 useful link function
 function sfml_links(tab, dbg, static)
-    local suffix = (static and "-s" or "") .. (dbg and "-d" or "")
-    for _, v in pairs(tab) do
-        links("sfml-" .. v .. suffix)
-    end
+  local suffix = (static and "-s" or "") .. (dbg and "-d" or "")
+  for _, v in pairs(tab) do
+    links("sfml-" .. v .. suffix)
+  end
 end
 
 -- ///////////////////////////////////////////////////// --
@@ -115,8 +115,8 @@ solution "SimpleGameEngine"
   
   if GPROF then
     configuration "GMake"
-  	  buildoptions { "-pg" }
-  	  linkoptions  { "-pg" }
+      buildoptions { "-pg" }
+      linkoptions  { "-pg" }
   end
 
   configuration "Debug"
@@ -137,7 +137,7 @@ project "SGE"
 
   sfml_libs = { "graphics", "audio", "window", "system" }
   if SFML_STATIC then
-  	defines { "SFML_STATIC" }
+    defines { "SFML_STATIC" }
   end
 
   configuration "Debug"
