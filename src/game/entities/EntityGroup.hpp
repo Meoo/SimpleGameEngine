a830bad4584ea0477a04e7a30055740b2584fbbf
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-10-28 14:58:59 +0100

    EntityGroup stub

---
+++ src/game/entities/EntityGroup.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file   EntityGroup.hpp
+ * @author Bastien Brunnenstein
+ */
+
+#ifndef _ENTITYGROUP_HPP_
+#define _ENTITYGROUP_HPP_
+
+#include "game/entities/Entity.hpp"
+
+/**
+ * A group of entities.
+ *
+ * Basically, it's a non solid, non visible and non active Entity.
+ */
+class EntityGroup : public Entity
+{
+public:
+                        EntityGroup(Entity * parent) : Entity(parent)
+                            { setActive(false); setSolid(false); setVisible(false); }
+
+    virtual             ~EntityGroup()
+                            {}
+
+    virtual Vector      getOrigin() const
+                            { return Vector(); }
+
+};
+
+#endif // _ENTITYGROUP_HPP_
